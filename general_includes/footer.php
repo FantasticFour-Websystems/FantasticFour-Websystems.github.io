d7953ac412efbaf42c72f255baf7f4b410c79ade
Emily Ngo <73451814+emmaleengo@users.noreply.github.com>
2021-10-14 21:22:10 -0400

    Add files via upload
    Created some php pages for repeat code - added some new pages for our product functions

---
+++ general_includes/footer.php
@@ -0,0 +1,29 @@
+<table id = "footer">
+<tr>
+  <td class="col">
+  Quick Links:<br>
+  <a class="footlink"href="index.html">Home</a><br>
+  <a class="footlink" href="events.html">Events/Activities</a><br>
+  <a class="footlink"href="contact.html">Contact Us</a><br>
+  </td>
+
+  <td class="col">
+  
+  Emails:<br>
+  <i class="fas fa-envelope-open ml-3\"></i><span>
+  <a class="footlink" href="href="mailto:INSERTHERE" class = "contact-button1" target="_blank"> President: smitha24@rpi.edu</a>
+  </span><br><i class="fas fa-envelope-open ml-3"></i><span>
+  <a class="footlink" href="href="mailto:INSERTHERE" class = "contact-button1"  target="_blank"> Organization: gammanueta@rpi.edu</a></span>
+  </td>
+
+  <td class="col">Social Media: <br>
+  <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+  <a href="#" class="social"><i class="fab fa-twitter"></i></a>
+  <a href="#" class="social"><i class="fab fa-tiktok"></i></a>
+  <a href="#" class="social"><i class="fab fa-instagram"></i></a>
+  <a href="#" class="social"><i class="fab fa-linkedin"></i></a>
+  </td>
+
+<tr>
+</table>
+
---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+  <body>
+  <?php include 'login/login_body.php';?>
+  </body>
+</html>
