1c76a8b4772b8117454fbc446445400704bbecfc
Tobey Tan <tant3@rpi.edu>
2021-12-09 17:46:31 -0500

    Update administrator.php

--- administrator.php
+++ administrator.php
@@ -8,10 +8,6 @@ if (!$conn) {
 
 ?>
 
-<?php
-if(isset($_POST[""]))
-?>
-
 <html>
  <head>
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
